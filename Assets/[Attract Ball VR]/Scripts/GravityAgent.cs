1f2fd7cc354bf2a27d0dd320396396a0c14c6e20
Clement <clement.roth@live.fr>
2019-12-03 12:57:15 +0100

    Ajout gravity controller prefab

---
+++ Assets/[Attract Ball VR]/Scripts/GravityAgent.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityAgent : MonoBehaviour
+{
+    private Rigidbody rb;
+    private ConstantForce cf;
+
+    public void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        gameObject.AddComponent<ConstantForce>();
+        cf = GetComponent<ConstantForce>();
+    }
+
+    public void ReverseGravity(bool gravityReversed)
+    {
+        if (gravityReversed)
+        {
+            rb.useGravity = false;
+            
+            cf.force = -Physics.gravity;
+        }
+        else
+        {
+            rb.useGravity = true;
+
+            cf.force = Vector3.zero;
+        }
+    }
+}
