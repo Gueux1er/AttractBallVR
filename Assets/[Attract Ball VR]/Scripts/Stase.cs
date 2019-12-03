084d59a3d88cc9a46f2f42addcd61e35757b19ee
lmoge <ludovic.moge@gmail.com>
2019-12-03 10:33:51 +0100

    - Staser (not finished but i guess i cannot program for shit)

---
+++ Assets/[Attract Ball VR]/Scripts/Stase.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class Stase : MonoBehaviour
+{
+    public bool active = true;
+    public StaseRange range = StaseRange.Local;
+
+
+    public SteamVR_Action_Single input;
+    public SteamVR_Input_Sources inputSource;
+
+    [Header("Local")]
+    public Transform referencePoint;
+    public float radiusOfEffect;
+
+    private void Start()
+    {
+        input.AddOnAxisListener(ActiveStase, inputSource);
+    }
+
+    private void ActiveStase(SteamVR_Action_Single fromInput, SteamVR_Input_Sources fromSource, float newAxis, float newDelta)
+    {
+        switch (range)
+        {
+            case StaseRange.Global:
+                break;
+
+            case StaseRange.Local:
+                break;
+        }
+
+    }
+
+    public enum StaseRange
+    {
+        Local, Global
+    }
+}
