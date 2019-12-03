9be856f2916a5fcdefab41e75774c96f495005de
giinsen <gabriel.lapegue@yahoo.fr>
2019-12-03 12:46:52 +0100

    Spawner manager prefab

---
+++ Assets/[Attract Ball VR]/Scripts/PoolManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoolManager : MonoBehaviour
+{    
+    public GameObject prefabContainer;
+    private List<GameObject> poolList = new List<GameObject>();
+
+    public int poolMax;
+
+    void Start()
+    {
+        for (int i = 0; i < prefabContainer.transform.childCount; i++)
+        {
+            poolList.Add(prefabContainer.transform.GetChild(i).gameObject);
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+}
