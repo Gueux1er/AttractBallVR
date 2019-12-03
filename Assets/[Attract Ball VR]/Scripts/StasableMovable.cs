3d3086cc0ba67f6c206470686c5b7a2732fda395
lmoge <ludovic.moge@gmail.com>
2019-12-03 14:48:43 +0100

    Stasable scripts and objects

---
+++ Assets/[Attract Ball VR]/Scripts/StasableMovable.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StasableMovable : MonoBehaviour
+{
+    public bool UnstaseOnCollision = true;
+
+    private Rigidbody rb;
+
+    public void Stase()
+    {
+        rb.velocity = Vector3.zero;
+        rb.isKinematic = true;
+    }
+
+    public void Unstase()
+    {
+        rb.isKinematic = false;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (UnstaseOnCollision)
+            Unstase();
+    }
+}
