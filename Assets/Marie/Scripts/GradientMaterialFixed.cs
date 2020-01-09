9ba25774ad39672f07cc9be37dc48be42df98ee6
lmoge <ludovic.moge@gmail.com>
2020-01-09 16:26:26 +0100

    Intégration scène de Marie

---
+++ Assets/Marie/Scripts/GradientMaterialFixed.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GradientMaterialFixed : MonoBehaviour
+{
+    public Gradient gradient;
+    private Material mat;
+    public int index;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mat = this.GetComponent<MeshRenderer>().materials[index];
+        mat.color = gradient.Evaluate(Random.Range(0.0f, 1.0f));
+    }
+
+}
